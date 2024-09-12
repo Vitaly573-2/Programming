5c7d21bc4964d78a364209535f2df0c02176531c
bokar <bokarevv801@gmail.com>
2024-09-12 11:36:19 +0600

    laba1.2

--- ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -31,7 +31,10 @@
             this.ItemtabControl = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.itemsTab1 = new ObjectOrientedPractics.View.Tabs.ItemsTab();
             this.ItemtabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ItemtabControl
@@ -41,15 +44,16 @@
             this.ItemtabControl.Location = new System.Drawing.Point(0, 0);
             this.ItemtabControl.Name = "ItemtabControl";
             this.ItemtabControl.SelectedIndex = 0;
-            this.ItemtabControl.Size = new System.Drawing.Size(823, 654);
+            this.ItemtabControl.Size = new System.Drawing.Size(738, 686);
             this.ItemtabControl.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.itemsTab1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(815, 628);
+            this.tabPage1.Size = new System.Drawing.Size(730, 660);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "items";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -60,21 +64,29 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(815, 628);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // itemsTab1
+            // 
+            this.itemsTab1.Location = new System.Drawing.Point(3, 3);
+            this.itemsTab1.Name = "itemsTab1";
+            this.itemsTab1.Size = new System.Drawing.Size(730, 656);
+            this.itemsTab1.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(818, 654);
+            this.ClientSize = new System.Drawing.Size(736, 687);
             this.Controls.Add(this.ItemtabControl);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ItemtabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -84,6 +96,8 @@
         private System.Windows.Forms.TabControl ItemtabControl;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private View.Tabs.ItemsTab itemsTab1;
     }
 }
 
