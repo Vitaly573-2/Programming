22edd8674b9e9e3bbf28c8e73a75196336750c46
bokar <bokarevv801@gmail.com>
2024-11-07 16:06:49 +0600

    We created the Time Range enumeration for the time indication, as well as the Priority Order class, where variables from the base class are inherited in the constructor

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/PriorityOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PriorityOrder : Order
+    {
+        private int _id;
+        private string _createData;
+        private OrderStatus _status;
+        private Address _address;
+        private List<Item> _items;
+
+        //желаемая дата
+        public DateTime DataTime { get; set; }
+        //желаемое время 
+        public TimeRange TimeRange { get; set; }
+
+        public PriorityOrder() { }
+
+        public PriorityOrder(int id, string createData, OrderStatus status,
+                             Address address, List<Item> items, DateTime dataTime, TimeRange timeRange)
+            :base(status, createData, items, address)
+        {
+            DataTime = dataTime;
+            TimeRange= timeRange;
+        }
+
+    }
+}
