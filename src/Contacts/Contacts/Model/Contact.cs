228782f2e4468a57ae249650b045c8ceb4d912e8
bokar <bokarevv801@gmail.com>
2025-02-27 14:07:12 +0600

    Создал класс Contact, реализовал поля, свойства и конструкторы

---
+++ src/Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    public class Contact
+    {
+        private string _name;
+        private int _number;
+        private string _email;
+    
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public string Email { get; set; }
+
+        public Contact()
+        {
+            Name = string.Empty;
+            Number = 0;
+            Email = string.Empty;
+        }
+
+        public Contact(string name, int number, string email)
+        {
+            Name = name;
+            Number = number;
+            Email = email;
+        }
+    }
+}
