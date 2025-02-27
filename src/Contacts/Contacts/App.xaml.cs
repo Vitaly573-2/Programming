49c7d4f5b386d59f669993765c4345f40717d122
bokar <bokarevv801@gmail.com>
2025-02-27 13:47:52 +0600

    Создал новое решение Contacts

---
+++ src/Contacts/Contacts/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Contacts
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
